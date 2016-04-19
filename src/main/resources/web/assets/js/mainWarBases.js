6dd7db8dc69f06f421c1a70afc4b49f8d8fe915a
Daniel Holmes <daniel@danielholmes.org>
2016-04-19 11:59:07 +1000

    Clan analysis ui tweaks

--- src/main/resources/web/assets/js/mainWarBases.js
+++ src/main/resources/web/assets/js/mainWarBases.js
@@ -21,38 +21,46 @@ $(document).ready(function() {
                                 )
                             ));
 
-                            return $('<div />').append($('<h3 />').html(pair[0]))
+                            var headRow = $('<tr />').append($('<th />').append('Name'));
+                            if (rules.length > 0) {
+                                headRow.append($('<th />').append('Report'))
+                                    .append(_.map(rules, function (rule) {
+                                        return $('<th />').append(rule);
+                                    }));
+                            }
+                            return $('<div />')
+                                .append($('<h4 />').html("Town Hall " + pair[0]))
                                 .append(
                                     $('<table />')
                                         .addClass("table")
                                         .addClass("table-bordered")
-                                        .append(
-                                            $('<thead />').append(
-                                                $('<tr />')
-                                                    .append($('<th />').append('Name'))
-                                                    .append($('<th />').append('Report'))
-                                                    .append(_.map(rules, function(rule) { return $('<th />').append(rule); }))
-                                            )
-                                        )
+                                        .append($('<thead />').append(headRow))
                                         .append(
                                             _.map(
                                                 pair[1],
                                                 function(report) {
                                                     var link = "/#" + encodeURI(report.userName) + "/war";
-                                                    var allSuccess = _.all(report['resultSummaries'], function(summary) { return summary['success']; });
+                                                    if (report['resultSummaries']) {
+                                                        var allSuccess = _.all(report['resultSummaries'], function(summary) { return summary['success']; });
+                                                        return $('<tr />')
+                                                            .addClass(allSuccess ? 'success' : 'fail')
+                                                            .append($('<td />').append(report.userName))
+                                                            .append($('<td />').append($('<a />').attr("href", link).attr("target", "_blank").append(link)))
+                                                            .append(
+                                                                _.map(rules, function(rule) {
+                                                                    var summary = _.find(report['resultSummaries'], function(s) { return s.name == rule; });
+                                                                    return $('<td />')
+                                                                        .append(
+                                                                            $('<span />').addClass('glyphicon')
+                                                                                .addClass(summary.success ? '' : 'glyphicon-remove-sign')
+                                                                        );
+                                                                })
+                                                            );
+                                                    }
+
                                                     return $('<tr />')
-                                                        .addClass(allSuccess ? 'success' : 'fail')
-                                                        .append($('<td />').append(report.userName))
-                                                        .append($('<td />').append($('<a />').attr("href", link).attr("target", "_blank").append(link)))
-                                                        .append(
-                                                            _.map(report['resultSummaries'], function(summary) {
-                                                                return $('<td />')
-                                                                    .append(
-                                                                        $('<span />').addClass('glyphicon')
-                                                                            .addClass(summary.success ? '' : 'glyphicon-remove-sign')
-                                                                    );
-                                                            })
-                                                        );
+                                                        .addClass('success')
+                                                        .append($('<td />').append(report.userName));
                                                 }
                                             )
                                         )
